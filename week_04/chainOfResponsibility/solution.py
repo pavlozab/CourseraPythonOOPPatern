bdcb86b5d88182e117a23a2e596e91c4786f620a
pavlozab <pavlo.zabavskyi@lnu.edu.ua>
2021-01-13 21:21:07 +0200

    update chain_of_responsibility

--- week_04/chainOfResponsibility/solution.py
+++ week_04/chainOfResponsibility/solution.py
@@ -1,18 +1,16 @@
-class SomeObject:
-    def __init__(self):
-        self.integer_field = 0
-        self.float_field = 0.0
-        self.string_field = ""
-        
+E_INT, E_FLOAT, E_STR = "INT", "FLOAT", "STR"
+
 
 class EventGet:
-    def __init__(self, obj_type):
-        self.obj_type = obj_type
+    def __init__(self, prop):
+        self.kind = {int:E_INT, float:E_FLOAT, str:E_STR}[prop];
+        self.prop = None;
 
 
 class EventSet:
-    def __init__(self, obj_data):
-        self.obj_data = obj_data
+    def __init__(self, prop):
+        self.kind = {int:E_INT, float:E_FLOAT, str:E_STR}[type(prop)];
+        self.prop = prop;
 
 
 class NullHandler:
@@ -26,51 +24,32 @@ class NullHandler:
 
 class IntHandler(NullHandler):
     def handle(self, obj, event):
-        if isinstance(event, EventGet) and (event.obj_type == int):
-            return obj.integer_field
-        elif isinstance(event, EventSet) and (type(event.obj_data) == int):
-            obj.integer_field = event.obj_data
+        if event.kind == E_INT:
+            if event.prop is None:
+                return obj.integer_field
+            else:
+                obj.integer_field = event.prop;
         else:
-            return super().handle(obj, event)  
-
-
-class FloatHandler(NullHandler):
-    def handle(self, obj, event):
-        if isinstance(event, EventGet) and (event.obj_type == float):
-            return obj.float_field
-        elif isinstance(event, EventSet) and (type(event.obj_data) == float):
-            obj.float_field = event.obj_data
-        else:
-            return super().handle(obj, event)  
+            return super().handle(obj, event)
 
 
 class StrHandler(NullHandler):
     def handle(self, obj, event):
-        if isinstance(event, EventGet) and (event.obj_type ==  str):
-            return obj.string_field
-        elif isinstance(event, EventSet) and (type(event.obj_data) == str):
-            obj.string_field = event.obj_data
+        if event.kind == E_STR:
+            if event.prop is None:
+                return obj.string_field
+            else:
+                obj.string_field = event.prop;
         else:
-            return super().handle(obj, event)  
+            return super().handle(obj, event)
 
 
-obj = SomeObject()
-obj.integer_field = 42
-obj.float_field = 3.14
-obj.string_field = "some text"
-chain = StrHandler(IntHandler(FloatHandler(NullHandler)))
-print(chain.handle(obj, EventGet(int)))
-# 42
-print(chain.handle(obj, EventGet(float)))
-# 3.14
-print(chain.handle(obj, EventGet(str)))
-# 'some text'
-chain.handle(obj, EventSet(100))
-print(chain.handle(obj, EventGet(int)))
-# 100
-chain.handle(obj, EventSet(0.5))
-print(chain.handle(obj, EventGet(float)))
-# 0.5
-chain.handle(obj, EventSet('new text'))
-print(chain.handle(obj, EventGet(str)))
-# 'new text'
+class FloatHandler(NullHandler):
+    def handle(self, obj, event):
+        if event.kind == E_FLOAT:
+            if event.prop is None:
+                return obj.float_field
+            else:
+                obj.float_field = event.prop;
+        else:
+            return super().handle(obj, event)
